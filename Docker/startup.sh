556d3d3f96cdb08be1170c33555fc5bb511c361e
shiltemann <s.hiltemann@erasmusmc.nl>
2016-01-07 16:37:37 +0100

    use php builtin webserver instead of python SimpleHTTPServer

--- Docker/startup.sh
+++ Docker/startup.sh
@@ -4,7 +4,8 @@ sed -i "s|PROXY_PREFIX|${PROXY_PREFIX}|" /proxy.conf;
 cp /proxy.conf /etc/nginx/sites-enabled/default;
 
 # Serve the Phinch website
-cd /home/Phinch && python -mSimpleHTTPServer &
+#cd /home/Phinch && python -mSimpleHTTPServer &
+cd /home/Phinch && php -S 127.0.0.1:8000 &
 
 # update filename to load in Phinch in readFile.js (if you use same name every time, chrome will use previously stored db)
 cd /home/Phinch/data
