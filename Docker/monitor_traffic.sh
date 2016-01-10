4928086fb05fadcc0e9f7c6cea3100b677c5cd19
shiltemann <zazkia@gmail.com>
2016-01-11 00:18:19 +0100

    kill container if last HTTP request was received more than 10 minutes ago

--- Docker/monitor_traffic.sh
+++ Docker/monitor_traffic.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# Take the netstat output the estimate if the client is still connected to
-# the RStudio server. The 'CLOSE_WAIT' state will be ignored. It
-# Indicates that the server has received the first FIN signal from the client
-# and the connection is in the process of being closed. But that can never happen.
-# For some reason there are a few connections open that do not relate the
-# client that needs to be connected over the port :80 If we do not have a
-# connection open from port 80, kill the server and herewith the docker container.
+# Check time of last http request in output from tcpdump. If it was recieved more than 
+# 10 minutes ago, stop the container
 
+timeout=600
 while true; do
     sleep 60
+  
+    lastrequesttime=`cat tcp.log | grep http | tail -1 | cut -f1 -d"."`
+    lastrequesttime=`date -d "$lastrequesttime" +%s`
+    now=`date +%s`
 
-    if [ `netstat -t | grep -v CLOSE_WAIT | grep ':80' | wc -l` -lt 3 ]
+    if [[ $now -gt $(($lastrequesttime+$timeout)) ]]   
     then
         pkill nginx
     fi
